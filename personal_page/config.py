6e0517ba9f21d767f9e1cbbb5a5fc7e5a53b7d76
Cleyson Lima <cleysonph@gmail.com>
2019-02-13 22:45:51 -0300

    Make some configs to deploy

--- personal_page/config.py
+++ personal_page/config.py
@@ -4,5 +4,5 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 SECRET_KEY = 'my-secret-key'
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
